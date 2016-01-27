281ff518db60e0508dc9b10e38ea4439859675b9
Ashton Moyer <Lordashtonthecool@hotmail.com>
2016-01-27 09:53:43 -0700

    Trying to update this

--- Assets/IfStatements.cs
+++ Assets/IfStatements.cs
@@ -2,14 +2,41 @@
 using System.Collections;
 
 public class IfStatements : MonoBehaviour {
+	//These are my variviables 
+	float officeHotTemp = 45.03f;
+	float officeColdTemp = 20.4f;
+	float officeRoomTemp = 60.03f;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		coffeeTemperature -= Time.deltaTime * 5f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	void TempTest ()
+	{
+		// If the coffee's temperature is greater than the hottest drinking temperature...
+		if(coffeeTemperature > hotLimitTemperature)
+		{
+			// ... do this.
+			print("Coffee is too hot.");
+		}
+		// If it isn't, but the coffee temperature is less than the coldest drinking temperature...
+		else if(coffeeTemperature < coldLimitTemperature)
+		{
+			// ... do this.
+			print("Coffee is too cold.");
+		}
+		// If it is neither of those then...
+		else
+		{
+			// ... do this.
+			print("Coffee is just right.");
+		}
 	}
 }
