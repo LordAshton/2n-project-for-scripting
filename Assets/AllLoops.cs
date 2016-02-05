7aa7d2689f1c63bed66fd738c3b9deae4e3d2e6f
Ashton Moyer <Lordashtonthecool@hotmail.com>
2016-02-05 09:46:04 -0700

    Update 02/05/2016

---
+++ Assets/AllLoops.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllLoops : MonoBehaviour {
+
+	float forLoops = 45.00f;
+	float whileLoops = 20.00f;
+	float doWhileLoops = 60.03f;
+	float foreachloop = 60.03f;
+	float variable = 60.03f;
+
+
+
+	// Use this for initialization
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TempTest();
+
+		variable -= Time.deltaTime * 5f;
+		//Debug.Log (officeTemp);
+	}
+
+
+	void TempTest ()
+	{
+
+		// for loop
+		if(variable > forLoops)
+		{
+
+
+		}
+		// while loops
+		else if(variable < whileLoops)
+		{
+
+
+		}
+
+		else if(variable < doWhileLoops)
+		{
+
+
+		}
+
+		//  and for the last one
+		else
+		{
+
+
+		}
+	}
+}
