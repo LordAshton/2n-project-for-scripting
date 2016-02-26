f9da0df162b40fac588ac3fdacbe6322c9fa8628
Ashton Moyer <Lordashtonthecool@hotmail.com>
2016-02-26 09:26:04 -0700

    Adding the Arrays and the data Types

---
+++ Assets/Arrayss.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrayss : MonoBehaviour 
+{
+	public GameObject[] players;
+
+	void Start ()
+	{
+		players = GameObject.FindGameObjectsWithTag("Player");
+
+		for(int i = 0; i < players.Length; i++)
+		{
+			Debug.Log("GameObject "+i+" is named "+players[i].name);
+		}
+	}
+}
