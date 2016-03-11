a33c0a41b382e32f431ae496e412f8adf9927df4
Ashton Moyer <Lordashtonthecool@hotmail.com>
2016-03-11 09:32:41 -0700

    Done with Switch statements

--- Assets/SwitchStatements.cs
+++ Assets/SwitchStatements.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class SwitchStatements : MonoBehaviour {
+	public int smarts = 5;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update()
+	{
+		switch (smarts)
+		{
+		case 5:
+			print ("Well Aren't we a mr. Smarty Pants Today?");
+			break;
+		case 4:
+			print ("Doing quite well today are we?");
+			break;
+		case 3:
+			print ("There Somein you want?");
+			break;
+		case 2:
+			print ("ME WANT COOKIE!");
+			break;
+		case 1:
+			print ("Mmmmurup, BUrp mmmmmm durrrrr");
+			break;
+		default:
+			print ("Yeah No, not happening");
+			break;
+		}
 	}
 }
